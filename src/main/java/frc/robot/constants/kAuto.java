daac837211001ac15c181baca3486a69b35a83c4
brennenputh <brennenputh@gmail.com>
2022-03-19 16:23:08 -0400

    Change constants to be more in tune with devbot

--- src/main/java/frc/robot/constants/kAuto.java
+++ src/main/java/frc/robot/constants/kAuto.java
@@ -6,9 +6,13 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 public class kAuto {
     private static final TrapezoidProfile.Constraints CONSTRAINTS = new TrapezoidProfile.Constraints(kSwerve.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND, kSwerve.MAX_VELOCITY_METERS_PER_SECOND/0.6);
-    public static final ProfiledPIDController THETA_PID_CONTROLLER = new ProfiledPIDController(4, 0, 0, CONSTRAINTS, 0.02);
+    public static final ProfiledPIDController THETA_PID_CONTROLLER = new ProfiledPIDController(3.5, 0, 0, CONSTRAINTS, 0.02);
 
-    public static final double XY_P = 0.1;
+    static {
+        THETA_PID_CONTROLLER.enableContinuousInput(-180, 180);
+    }
+
+    public static final double XY_P = 1;
 
     public static final PIDController X_PID_CONTROLLER = new PIDController(XY_P, 0, 0);
     public static final PIDController Y_PID_CONTROLLER = new PIDController(XY_P, 0, 0);
